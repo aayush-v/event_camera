1eb2d6965db7c8003fd799347a5fd74e4f2913f7
Aayush <aayushatul@gmail.com>
2023-09-29 18:07:12 -0700

    updated util scripts

--- scripts/rvt_dataset_prep/hdf5_file_handling/convert_hdf5_1.py
+++ scripts/rvt_dataset_prep/hdf5_file_handling/convert_hdf5_1.py
@@ -1,36 +1,41 @@
 import h5py
 import numpy as np
 import time
+import os
+import glob
 
 # Convert the hdf5 file from metavision format to only events format 
-start_time = time.time()
+def convertedHDF5(original_path):
+    start_time = time.time()
 
-original_file_path = '/media/exx/Elements/aayush/rvt/test1/h5exp/singleHandWaving_New_activity.hdf5'
-final_path = 'new_file.hdf5'
+    with h5py.File(original_path, "r") as f:
+        final_path = '/media/exx/Elements/aayush/rvt/dataset/h5_files/new_' + original_path.split('/')[-1]
 
-with h5py.File(original_file_path, "r") as f:
-    final = h5py.File(final_path, "w")
-    chunk_shape = (16384,)
-    evts = f["CD"]
-    group = final.create_group("events")
+        final = h5py.File(final_path, "w")
+        evts = f["CD"]
+        group = final.create_group("events")
 
+        print(evts['events']['x'].size)
+        xdataset = group.create_dataset('x', data = evts['events']['x'], chunks=True, compression="gzip",) 
+        ydataset = group.create_dataset('y', data = evts['events']['y'], chunks=True, compression="gzip")
+        pdataset = group.create_dataset('p', data = evts['events']['p'], chunks=True, compression="gzip")
+        tdataset = group.create_dataset('t', data = evts['events']['t'], chunks=True, compression="gzip")
+        hdataset = group.create_dataset('height', data = 720)
+        wdataset = group.create_dataset('width', data = 1280)
 
-    print(evts['events']['x'].size)
-    xdataset = group.create_dataset('x', data = evts['events']['x'], chunks=True, compression="gzip",) 
-    ydataset = group.create_dataset('y', data = evts['events']['y'], chunks=True, compression="gzip")
-    pdataset = group.create_dataset('p', data = evts['events']['p'], chunks=True, compression="gzip")
-    tdataset = group.create_dataset('t', data = evts['events']['t'], chunks=True, compression="gzip")
-    dividerdataset = group.create_dataset('divider', data = 1)
-    hdataset = group.create_dataset('height', data = 1280)
-    wdataset = group.create_dataset('width', data = 720)
+        final.close()
 
-    final.close()
+    end_time = time.time()
 
-end_time = time.time()
-
-
-elapsed_time = end_time - start_time
-print(f"Elapsed time: {elapsed_time:.2f} seconds")
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time: {elapsed_time:.2f} seconds")
 
 
+if __name__ == "__main__":
+
+    original_folder = '/media/exx/Elements/aayush/data/h5data/tmpfodler'
+    h5_files = glob.glob(os.path.join(original_folder, '*.hdf5'))
+
+    for original_path in h5_files:
+        convertedHDF5(original_path)
 
--- scripts/rvt_dataset_prep/npy_file_handling/convert_npy.py
+++ scripts/rvt_dataset_prep/npy_file_handling/convert_npy.py
@@ -1,4 +1,9 @@
+"""
+Convert metavision annotation format to RVT format
+"""
 import numpy as np
+import os
+import glob
 
 
 def convert_npy(npy_file):
@@ -17,10 +22,17 @@ def convert_npy(npy_file):
     converted_npy['class_confidence'] = old_npy['class_confidence']
     converted_npy['track_id'] = old_npy['track_id']
     print("NPY format updated")
-    np.save('new_npy.npy', converted_npy)
+
+    converted_npy_path = '/media/exx/Elements/aayush/rvt/dataset/annotations/new_' +  npy_file.split('/')[-1]
+    np.save(converted_npy_path, converted_npy)
 
 if __name__ == "__main__":
-    # npy_file = "/media/exx/Elements/aayush/1MPX_data/test/17-04-04_11-00-13_cut_15_183500000_243500000_bbox.npy"
-    npy_file = "/media/exx/E4D8-27DA/Event_data/july27_working_dir/raw_data/labels_output/nighttime/19-50-15_bbox.npy"
-    convert_npy(npy_file)
+    annotations_folder = "/media/exx/E4D8-27DA/Event_data/july27_working_dir/raw_data/labels_output"
+    npy_files = glob.glob(os.path.join(annotations_folder, '*.npy'))
+
+    for file in npy_files:   
+        convert_npy(file)
+
+    
+    # convert_npy(npy_file)
 
